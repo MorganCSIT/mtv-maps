3bcbd3d5df858e5184295cbd6cf5e64128bf12a1
Morgan <morgankieffer@gmail.com>
2023-12-02 10:45:36 +0700

    combined data objects

--- my-maps-app/src/App.jsx
+++ my-maps-app/src/App.jsx
@@ -8,75 +8,67 @@ function App() {
   const [selectedMap, setSelectedMap] = useState(null);
   const [selectedArticle, setSelectedArticle] = useState(null);
 
-  const mapOptions = [
+  const mapsData = [
     {
-      value:
+      id: 1,
+      mapUrl:
         "https://www.google.com/maps/d/embed?mid=1yEadiJSV3W_90XMluZyERkIEXRE&hl=en&ehbc=2E312F",
       label: "Phuket Full Map",
-      articleId: 1,
+      article: {
+        heading: "Full Map",
+        dateUpdated: "2023-12-01",
+        content: (
+          <div>
+            <p>Information about Phuket...</p>
+            <ul>
+              <li>Detail 1</li>
+              <li>Detail 2</li>
+            </ul>
+            {/* More JSX content */}
+          </div>
+        ),
+        links: [{ url: "#", text: "Read more" }],
+      },
     },
     {
-      value:
+      id: 2,
+      mapUrl:
         "https://www.google.com/maps/d/embed?mid=19UESSqs4lEfrjIGcs1ICBqI6nPA&ehbc=2E312F",
       label: "Phuket Beaches",
-      articleId: 2,
+      article: {
+        heading: "Phuket Beaches",
+        dateUpdated: "2023-12-01",
+        content: (
+          <div>
+            <p>Our favourite beaches...</p>
+            {/* More JSX content */}
+          </div>
+        ),
+        links: [{ url: "#", text: "Read more" }],
+      },
     },
-    {
-      value:
-        "https://www.google.com/maps/d/embed?mid=1NVyHmS2XKonHZXVaDPKQd9RSIkZCF4bO&ehbc=2E312F",
-      label: "Piers and Marinas",
-      articleId: 3,
-    },
-    // Add more map options here
+    // Add more map data here
   ];
 
-  const articles = {
-    1: {
-      heading: "Phuket Full Map",
-      dateUpdated: "2023-12-01",
-      content: "Content about New York...",
-      links: [
-        {
-          url: "https://www.google.com/maps/d/embed?mid=1yEadiJSV3W_90XMluZyERkIEXRE&hl=en&ehbc=2E312F",
-          text: "Read more",
-        },
-      ],
-    },
-    2: {
-      heading: "Phuket Beaches",
-      dateUpdated: "2023-12-01",
-      content:
-        " Patong is Phuket's most famous beach, known for its vibrant nightlife, shopping, and wide beachfront. It's a great spot for those looking for entertainment and a lively atmosphere.Kata Beach is popular for its picturesque scenery, clear waters, and palm-lined beach. It's a family-friendly location with a relaxed vibe, suitable for swimming and surfing.Surin Beach is a stylish and upscale destination known for its fine white sand and turquoise waters. It's surrounded by high-end resorts, restaurants, and shops, making it a hotspot for luxury travelers.",
-      links: [
-        {
-          url: "https://www.google.com/maps/d/embed?mid=19UESSqs4lEfrjIGcs1ICBqI6nPA&ehbc=2E312F",
-          text: "Read more",
-        },
-      ],
-    },
-    3: {
-      heading: "Piers and Marinas",
-      dateUpdated: "2023-12-01",
-      content: "Content about London...",
-      links: [
-        {
-          url: "https://www.google.com/maps/d/embed?mid=19UESSqs4lEfrjIGcs1ICBqI6nPA&ehbc=2E312F",
-          text: "Read more",
-        },
-      ],
-    },
-    // Add more articles here
-  };
-
   const handleMapChange = (option) => {
-    setSelectedMap(option.value);
-    setSelectedArticle(articles[option.articleId]);
+    const selectedData = mapsData.find((map) => map.label === option.label);
+    if (selectedData) {
+      setSelectedMap(selectedData.mapUrl);
+      setSelectedArticle(selectedData.article);
+    }
   };
 
   return (
     <div className="App">
       <div className="pair-container">
-        <TagCarousel options={mapOptions} onChange={handleMapChange} />
+        <TagCarousel
+          options={mapsData.map((map) => ({
+            value: map.mapUrl,
+            label: map.label,
+          }))}
+          onChange={handleMapChange}
+        />
+
         <div className="content-container">
           {selectedMap && <MapDisplay mapUrl={selectedMap} />}
           <Article article={selectedArticle} />
