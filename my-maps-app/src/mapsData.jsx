2d9a026157c69e8c7c2ef2421d1af17260e616af
Morgan <morgankieffer@gmail.com>
2023-12-02 17:27:32 +0700

    fixed widescreen resolution and added more data objects

--- my-maps-app/src/mapsData.jsx
+++ my-maps-app/src/mapsData.jsx
@@ -3,13 +3,13 @@ const mapsData = [
     id: 1,
     mapUrl:
       "https://www.google.com/maps/d/embed?mid=1yEadiJSV3W_90XMluZyERkIEXRE&hl=en&ehbc=2E312F",
-    label: "Phuket Full Map",
+    label: "Aoyon essentials",
     article: {
-      heading: "Full Map",
+      heading: "Aoyon essentials",
       dateUpdated: "2023-12-01",
       content: (
         <div>
-          <p>Information about Phuket...</p>
+          <p>Things to know around the area...</p>
           <ul>
             <li>Detail 1</li>
             <li>Detail 2</li>
@@ -24,9 +24,9 @@ const mapsData = [
     id: 2,
     mapUrl:
       "https://www.google.com/maps/d/embed?mid=19UESSqs4lEfrjIGcs1ICBqI6nPA&ehbc=2E312F",
-    label: "Phuket Beaches",
+    label: "Beaches",
     article: {
-      heading: "Phuket Beaches",
+      heading: "Phukets Beaches",
       dateUpdated: "2023-12-01",
       content: (
         <div>
@@ -37,6 +37,92 @@ const mapsData = [
       links: [{ url: "#", text: "Read more" }],
     },
   },
+  {
+    id: 3,
+    mapUrl:
+      "https://www.google.com/maps/d/embed?mid=1EpT1999Heig7bEY3-WD6iO0Fb310sXMS&ehbc=2E312F",
+    label: "Romantic restuarants",
+    article: {
+      heading: "Adventure Trails in Phuket",
+      dateUpdated: "2023-12-01",
+      content: (
+        <div>
+          <p>Ticke your taste buds whilst enjoying life with your partner</p>
+          {/* More JSX content */}
+        </div>
+      ),
+      links: [{ url: "#", text: "Adventure Guide" }],
+    },
+  },
+  {
+    id: 4,
+    mapUrl:
+      "https://www.google.com/maps/d/embed?mid=1Ids1HeUTN1OEWOa_g3SyNE1lDMs&ehbc=2E312F",
+    label: "Markets",
+    article: {
+      heading: "Phukets popular markets",
+      dateUpdated: "2023-12-01",
+      content: (
+        <div>
+          <p>Sample the street food scene in Phuket...</p>
+          {/* More JSX content */}
+        </div>
+      ),
+      links: [{ url: "#", text: "Discover Street Food" }],
+    },
+  },
+  {
+    id: 5,
+    mapUrl:
+      "https://www.google.com/maps/d/embed?mid=1RaTFimNmsOm9XLN6gk5Q3GpgqMA&ehbc=2E312F",
+    label: "Phuket Nightlife Hotspots",
+    article: {
+      heading: "Phuket Nightlife",
+      dateUpdated: "2023-12-01",
+      content: (
+        <div>
+          <p>Experience the vibrant nightlife of Phuket...</p>
+          {/* More JSX content */}
+        </div>
+      ),
+      links: [{ url: "#", text: "Nightlife in Phuket" }],
+    },
+  },
+  {
+    id: 6,
+    mapUrl:
+      "https://www.google.com/maps/d/embed?mid=1r41LHPxZ-xopGzV84PC6MPl7Siw&ehbc=2E312F",
+    label: "Phuket Shopping Centers",
+    article: {
+      heading: "Shopping in Phuket",
+      dateUpdated: "2023-12-01",
+      content: (
+        <div>
+          <p>Explore shopping destinations in Phuket...</p>
+          {/* More JSX content */}
+        </div>
+      ),
+      links: [{ url: "#", text: "Phuket Shopping Guide" }],
+    },
+  },
+  {
+    id: 7,
+    mapUrl:
+      "https://www.google.com/maps/d/embed?mid=1xKUsu5P0pO5EoVFMSCdM1olt7qHATTUa&ehbc=2E312F",
+    label: "Phuket Cultural Sites",
+    article: {
+      heading: "Cultural Sites in Phuket",
+      dateUpdated: "2023-12-01",
+      content: (
+        <div>
+          <p>Visit the cultural and historical sites of Phuket...</p>
+          {/* More JSX content */}
+        </div>
+      ),
+      links: [{ url: "#", text: "Cultural Tours" }],
+    },
+  },
+
   // Add more map data here
 ];
 
