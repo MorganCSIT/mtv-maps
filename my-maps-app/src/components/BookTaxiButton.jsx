a4d90d5b25324d50927577d81638439cd03e7fc9
Morgan <morgankieffer@gmail.com>
2023-12-17 02:42:22 +0700

    added new dummy ifno and fixed whatsapp functionality

--- my-maps-app/src/components/BookTaxiButton.jsx
+++ my-maps-app/src/components/BookTaxiButton.jsx
@@ -10,49 +10,45 @@ const BookTaxiButton = () => {
     event.preventDefault();
     const message = `Pickup Point: ${pickupPoint}, Destination: ${destination}, Pickup Time: ${pickupTime}`;
     const encodedMessage = encodeURIComponent(message);
-    const whatsappUrl = `https://wa.me/1234567890?text=${encodedMessage}`; // replace 1234567890 with the actual number
+    const whatsappUrl = `https://wa.me/+66810851211?text=${encodedMessage}`;
     window.open(whatsappUrl, "_blank");
   };
 
   return (
     <div>
-      <button
-        style={{ marginBottom: 10 }}
-        onClick={() => setShowForm(!showForm)}
-      >
+      <button onClick={() => setShowForm(!showForm)}>
         {showForm ? "Hide Form" : "Show Form"}
       </button>
 
       {showForm && (
-        <form className="book-taxi-form">
-          <form onSubmit={handleSubmit} className="book-taxi-form">
-            <input
-              type="text"
-              value={pickupPoint}
-              onChange={(e) => setPickupPoint(e.target.value)}
-              placeholder="Pickup Point"
-              required
-              className="book-taxi-input"
-            />
-            <input
-              type="text"
-              value={destination}
-              onChange={(e) => setDestination(e.target.value)}
-              placeholder="Destination"
-              required
-              className="book-taxi-input"
-            />
-            <input
-              type="time"
-              value={pickupTime}
-              onChange={(e) => setPickupTime(e.target.value)}
-              required
-              className="book-taxi-input"
-            />
-            <button type="submit" className="book-taxi-button">
-              Book Taxi
-            </button>
-          </form>
+        <form className="book-taxi-form" onSubmit={handleSubmit}>
+          {/* Remove the extra form element */}
+          <input
+            type="text"
+            value={pickupPoint}
+            onChange={(e) => setPickupPoint(e.target.value)}
+            placeholder="Pickup Point"
+            required
+            className="book-taxi-input"
+          />
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder="Destination"
+            required
+            className="book-taxi-input"
+          />
+          <input
+            type="time"
+            value={pickupTime}
+            onChange={(e) => setPickupTime(e.target.value)}
+            required
+            className="book-taxi-input"
+          />
+          <button type="submit" className="book-taxi-button">
+            Book Taxi
+          </button>
         </form>
       )}
     </div>
--- my-maps-app/src/mapsData.jsx
+++ my-maps-app/src/mapsData.jsx
@@ -9,81 +9,40 @@ const mapsData = [
       dateUpdated: "2023-12-01",
       content: (
         <div>
-          <p>Things to know around the area...</p>
+          <h2>Welcome to Aoyon!</h2>
+          <p>
+            Aoyon, known as the "Emerald of the East", is a vibrant city with a
+            rich history and a thriving culture. Nestled between the majestic
+            Mount Zephyr and the sparkling Azure Sea, Aoyon offers a unique
+            blend of natural beauty and urban charm.
+          </p>
+          <h3>Things to know around the area:</h3>
           <ul>
-            <li>Detail 1</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
-            <li>Detail 2</li>
+            <li>
+              <strong>The Aoyon Market:</strong> A bustling hub of activity, the
+              Aoyon Market is the perfect place to sample local delicacies, buy
+              handmade crafts, and immerse yourself in the local culture.
+            </li>
+            <li>
+              <strong>Mount Zephyr:</strong> A hike up Mount Zephyr is a must
+              for nature lovers. The trail offers breathtaking views of the city
+              and the sea, and the summit is the perfect spot for a picnic.
+            </li>
+            <li>
+              <strong>The Azure Sea:</strong> The Azure Sea is known for its
+              clear blue waters and abundant marine life. It's a popular spot
+              for swimming, snorkeling, and boat tours.
+            </li>
+            <li>
+              <strong>The Aoyon Museum:</strong> The Aoyon Museum houses a vast
+              collection of artifacts that tell the story of the city's history
+              and culture. Don't miss the museum's famous "Emerald Exhibit".
+            </li>
           </ul>
-          {/* More JSX content */}
+          <p>
+            Whether you're here for a day or a month, Aoyon has something to
+            offer everyone. We hope you enjoy your stay in our beautiful city!
+          </p>
         </div>
       ),
       links: [{ url: "#", text: "Read more" }],
@@ -95,17 +54,47 @@ const mapsData = [
       "https://www.google.com/maps/d/embed?mid=19UESSqs4lEfrjIGcs1ICBqI6nPA&ehbc=2E312F",
     label: "Beaches",
     article: {
-      heading: "Phukets Beaches",
+      heading: "Phuket's Beaches",
       dateUpdated: "2023-12-01",
       content: (
         <div>
-          <p>Our favourite beaches...</p>
+          <h2>Discover the Best Beaches in Phuket</h2>
+          <p>
+            Phuket, Thailand's largest island, is famous for its stunning
+            beaches. Here are our top picks:
+          </p>
           <ol>
             <li>
-              Aoyon, obviously!
-              <p>Thers an undeniable magic in Aoyon</p>
+              <strong>Aoyon Beach:</strong> Tucked away in a quiet corner of the
+              island, Aoyon Beach is a hidden gem. With its soft white sand,
+              clear blue waters, and lush greenery, there's an undeniable magic
+              in Aoyon. It's the perfect place to relax and unwind away from the
+              crowds.
+            </li>
+            <li>
+              <strong>Patong Beach:</strong> Known for its vibrant nightlife,
+              Patong Beach is the place to be if you're looking for fun and
+              excitement. During the day, you can enjoy a variety of water
+              sports, and as the sun sets, the beach transforms into a bustling
+              party scene.
+            </li>
+            <li>
+              <strong>Kata Beach:</strong> Ideal for families, Kata Beach offers
+              a more laid-back vibe. The beach is lined with restaurants and
+              shops, and the shallow waters make it a safe place for children to
+              swim.
+            </li>
+            <li>
+              <strong>Surin Beach:</strong> If you're a fan of surfing, don't
+              miss Surin Beach. The waves here are perfect for surfing, and
+              there are plenty of surf shops where you can rent equipment or
+              take lessons.
             </li>
           </ol>
+          <p>
+            With so many beautiful beaches to choose from, you're sure to find
+            your perfect spot in the sun in Phuket!
+          </p>
         </div>
       ),
       links: [{ url: "#", text: "Read more" }],
@@ -115,17 +104,46 @@ const mapsData = [
     id: 3,
     mapUrl:
       "https://www.google.com/maps/d/embed?mid=1EpT1999Heig7bEY3-WD6iO0Fb310sXMS&ehbc=2E312F",
-    label: "Romantic restuarants",
+    label: "Phuket's Romantic Restaurants",
     article: {
-      heading: "Adventure Trails in Phuket",
+      heading: "Romantic Dining in Phuket",
       dateUpdated: "2023-12-01",
       content: (
         <div>
-          <p>Ticke your taste buds whilst enjoying life with your partner</p>
-          {/* More JSX content */}
+          <h2>Experience Romantic Dining in Phuket</h2>
+          <p>
+            Tickle your taste buds whilst enjoying life with your partner at
+            these romantic dining spots:
+          </p>
+          <ul>
+            <li>
+              <strong>La Gritta:</strong> Known for its stunning sea views and
+              Italian cuisine, La Gritta offers a romantic dining experience
+              like no other.
+            </li>
+            <li>
+              <strong>Acqua Restaurant:</strong> With its modern decor and
+              innovative dishes, Acqua Restaurant is the perfect place for
+              couples who appreciate fine dining.
+            </li>
+            <li>
+              <strong>Blue Elephant:</strong> Housed in a charming colonial
+              building, Blue Elephant serves authentic Thai cuisine in a
+              romantic setting.
+            </li>
+            <li>
+              <strong>Twilight Sky:</strong> If you're looking for a dining
+              experience with a view, Twilight Sky is the place to be. Enjoy a
+              meal as you watch the sun set over the Andaman Sea.
+            </li>
+          </ul>
+          <p>
+            With its beautiful settings and exquisite cuisine, Phuket is the
+            perfect place for a romantic dinner date!
+          </p>
         </div>
       ),
-      links: [{ url: "#", text: "Adventure Guide" }],
+      links: [{ url: "#", text: "Dining Guide" }],
     },
   },
   {
@@ -134,15 +152,82 @@ const mapsData = [
       "https://www.google.com/maps/d/embed?mid=1Ids1HeUTN1OEWOa_g3SyNE1lDMs&ehbc=2E312F",
     label: "Markets",
     article: {
-      heading: "Phukets popular markets",
+      heading: "Explore Phuket's Popular Markets",
       dateUpdated: "2023-12-01",
       content: (
         <div>
-          <p>Sample the street food scene in Phuket...</p>
-          {/* More JSX content */}
+          <h2>Experience the Vibrant Market Scene in Phuket</h2>
+          <p>
+            Sample the street food scene, shop for local crafts, and immerse
+            yourself in the local culture at these popular markets in Phuket:
+          </p>
+          <ul>
+            <li>
+              <strong>
+                <a
+                  href="https://www.phuket.com/shopping/phuket-weekend-market.htm"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Phuket Weekend Market:
+                </a>
+              </strong>{" "}
+              Also known as Naka Market, it's the largest market in Phuket.
+              You'll find everything from street food to clothing and souvenirs
+              here.
+            </li>
+            <li>
+              <strong>
+                <a
+                  href="https://www.phuket.com/shopping/banzaan-market.htm"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Banzaan Market:
+                </a>
+              </strong>{" "}
+              A great place to sample local Thai food. The market is clean,
+              well-organized, and offers a wide variety of fresh produce.
+            </li>
+            <li>
+              <strong>
+                <a
+                  href="https://www.phuket.com/shopping/malint-market-patong.htm"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Malin Plaza Patong:
+                </a>
+              </strong>{" "}
+              Known for its food stalls offering Thai and international dishes.
+              It's a great place to have dinner and do some shopping.
+            </li>
+            <li>
+              <strong>
+                <a
+                  href="https://www.phuket.com/shopping/chillva-market.htm"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Chillva Market:
+                </a>
+              </strong>{" "}
+              A trendy night market with a bohemian vibe. You'll find a mix of
+              food, fashion, and live music here.
+            </li>
+          </ul>
+          <p>
+            Don't miss the chance to explore these markets and discover the rich
+            flavors and vibrant culture of Phuket!
+          </p>
         </div>
       ),
-      links: [{ url: "#", text: "Discover Street Food" }],
+      links: [
+        {
+          url: "https://www.phuket.com/shopping/top10-markets.htm",
+          text: "Discover More Markets",
+        },
+      ],
     },
   },
   {
@@ -155,11 +240,33 @@ const mapsData = [
       dateUpdated: "2023-12-01",
       content: (
         <div>
-          <p>Experience the vibrant nightlife of Phuket...</p>
-          {/* More JSX content */}
+          <h2>Experience the Vibrant Nightlife of Phuket</h2>
+          <p>
+            From beach clubs to night markets, here are some of the best
+            nightlife hotspots in Phuket:
+          </p>
+          <ul>
+            <li>
+              <strong>Bangla Road:</strong> The heart of Phuket's nightlife,
+              Bangla Road is filled with bars, clubs, and live music venues.
+            </li>
+            <li>
+              <strong>Cape Coral:</strong> A stylish beach club offering
+              stunning sea views, delicious cocktails, and live DJ sets.
+            </li>
+            <li>
+              <strong>Night Markets:</strong> For a more local experience, visit
+              the night markets where you can sample street food, shop for
+              souvenirs, and enjoy live entertainment.
+            </li>
+          </ul>
+          <p>
+            Experience the vibrant nightlife of Phuket and make unforgettable
+            memories!
+          </p>
         </div>
       ),
-      links: [{ url: "#", text: "Nightlife in Phuket" }],
+      links: [{ url: "#", text: "Discover More Nightlife Hotspots" }],
     },
   },
   {
@@ -172,8 +279,31 @@ const mapsData = [
       dateUpdated: "2023-12-01",
       content: (
         <div>
-          <p>Explore shopping destinations in Phuket...</p>
-          {/* More JSX content */}
+          <h2>Explore Shopping Destinations in Phuket</h2>
+          <p>
+            From local markets to luxury malls, here are some of the best places
+            to shop in Phuket:
+          </p>
+          <ul>
+            <li>
+              <strong>Jungceylon Shopping Mall:</strong> Located in Patong,
+              Jungceylon is one of the biggest shopping complexes in Phuket.
+            </li>
+            <li>
+              <strong>Central Festival Phuket:</strong> This is the place to go
+              for international brands, a wide range of dining options, and
+              entertainment facilities.
+            </li>
+            <li>
+              <strong>Phuket Weekend Market:</strong> For a more local shopping
+              experience, visit the Phuket Weekend Market where you can find
+              everything from clothes to street food.
+            </li>
+          </ul>
+          <p>
+            Explore the diverse shopping destinations in Phuket and find unique
+            souvenirs to take home!
+          </p>
         </div>
       ),
       links: [{ url: "#", text: "Phuket Shopping Guide" }],
@@ -189,11 +319,35 @@ const mapsData = [
       dateUpdated: "2023-12-01",
       content: (
         <div>
-          <p>Visit the cultural and historical sites of Phuket...</p>
-          {/* More JSX content */}
+          <h2>Visit the Cultural and Historical Sites of Phuket</h2>
+          <p>
+            Immerse yourself in the rich history and culture of Phuket at these
+            sites:
+          </p>
+          <ul>
+            <li>
+              <strong>Big Buddha:</strong> This 45-meter tall statue is one of
+              the most famous landmarks in Phuket, offering panoramic views of
+              the island.
+            </li>
+            <li>
+              <strong>Old Phuket Town:</strong> Explore the beautifully
+              preserved Sino-Portuguese architecture in Old Phuket Town, a
+              testament to the island's rich cultural heritage.
+            </li>
+            <li>
+              <strong>Wat Chalong:</strong> Visit Wat Chalong, the largest and
+              most important Buddhist temple in Phuket, known for its beautiful
+              pagoda and intricate murals.
+            </li>
+          </ul>
+          <p>
+            Discover the cultural and historical treasures of Phuket and gain a
+            deeper understanding of the island's heritage!
+          </p>
         </div>
       ),
-      links: [{ url: "#", text: "Cultural Tours" }],
+      links: [{ url: "#", text: "Explore More Cultural Sites" }],
     },
   },
 
