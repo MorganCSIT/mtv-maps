b7d25e54ceeb7354ab122b33d40006167ca843b2
Morgan <morgankieffer@gmail.com>
2024-04-15 14:45:33 +0700

    fixed state error with tag carousel

--- my-maps-app/src/App.jsx
+++ my-maps-app/src/App.jsx
@@ -1,54 +1,67 @@
-import React, { useState } from "react";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useParams
+} from "react-router-dom";
 import MapDisplay from "./components/MapDisplay";
 import TagCarousel from "./components/TagCarousel";
 import Article from "./components/Article";
 import "./App.css";
 import mapsData from "./mapsData";
-import BookTaxiButton from "./components/BookTaxiButton";
+
 
 function App() {
-  const [selectedMap, setSelectedMap] = useState(null);
-  const [selectedArticle, setSelectedArticle] = useState(null);
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/map/:mapId" element={<MapRoute />} />
+          <Route path="*" element={<Navigate replace to={`/map/${mapsData[0].id}`} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
-  const handleMapChange = (options) => {
-    if (options.length > 0) {
-      const selectedOption = options[0];
-      const selectedData = mapsData.find(
-        (map) => map.label === selectedOption.label
-      );
-      if (selectedData) {
-        setSelectedMap(selectedData.mapUrl);
-        setSelectedArticle(selectedData.article);
-      }
-    }
-  };
+function MapRoute() {
+  const { mapId } = useParams();
+  const mapData = mapsData.find(data => data.id.toString() === mapId);
+
+  if (!mapData) {
+    return <Navigate replace to="/" />;
+  }
+
+  const selectedOption = [{
+    value: mapId,
+    label: mapData.label // Ensure this label matches exactly one in the options
+  }];
 
   return (
-    <div className="App">
-      <div className="">
-        <div className="pair-container">
-          <div style={{ textAlign: "center" }}>
-            <h2>
-              <em>Explore Phuket!</em>
-            </h2>
-            <p>
-              <em> "Using a catergorized list of locally curated maps"</em>
-            </p>
-          </div>
-          <div className="content-container">
-            {selectedMap && <MapDisplay mapUrl={selectedMap} />}
-            <TagCarousel
-              options={mapsData.map((map) => ({
-                value: map.mapUrl,
-                label: map.label,
-              }))}
-              onChange={handleMapChange}
-            />
-            <Article article={selectedArticle} />
-          </div>
-          <br></br>
-        </div>
+    <div className="pair-container">
+      <div style={{ textAlign: "center" }}>
+        <h2><em>Explore Phuket!</em></h2>
+        <p><em>"Using a categorized list of locally curated maps"</em></p>
       </div>
+      <div className="content-container">
+        <MapDisplay mapUrl={mapData.mapUrl} />
+        <TagCarousel
+          options={mapsData.map(map => ({
+            value: map.id.toString(),
+            label: map.label
+          }))}
+          selectedValue={selectedOption}
+          onChange={(selectedOptions) => {
+            if (selectedOptions.length > 0) {
+              window.location.href = `/map/${selectedOptions[0].value}`;
+            }
+          }}
+        />
+        <Article article={mapData.article} />
+      </div>
+      <br></br>
     </div>
   );
 }
--- my-maps-app/src/components/TagCarousel.jsx
+++ my-maps-app/src/components/TagCarousel.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-// import Select from "react-select";
 import Dropdown from "react-dropdown-select";
 
-const TagCarousel = ({ options, onChange }) => {
+const TagCarousel = ({ options, onChange, selectedValue }) => {
+  console.log("Options:", options); // Check what options you are getting
+  console.log("Selected Value:", selectedValue); // Check what selectedValue you are getting
+
   return (
     <Dropdown
       className="carousel"
       options={options}
+      values={selectedValue}
       searchable={false}
-      onChange={(selectedOption) => onChange(selectedOption)}
+      onChange={(selectedOptions) => onChange(selectedOptions)}
     />
   );
 };
 
 export default TagCarousel;
+
+
 
 // const TagCarousel = ({ options, onChange }) => {
 //   return (
